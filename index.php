1fd83f1aea822ece2c9e36b5f92733e2240961b6
Nestea <72440100+Nestea29950@users.noreply.github.com>
2024-06-25 09:29:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Kartennco
+ * Plugin URI: http://example.com/kartennco
+ * Description: Une description courte de votre extension.
+ * Version: 1.0
+ * Author: Votre Nom
+ * Author URI: http://example.com
+ * License: GPL2
+ */
